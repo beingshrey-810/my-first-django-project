e6fe330c944dad324e29cd6a801eec2203ddd492
Shrey Maradia <beingshrey810@gmail.com>
2025-01-08 09:11:45 +0530

    Styling Changes

--- blogs/forms.py
+++ blogs/forms.py
@@ -6,12 +6,14 @@ from .models import Blog
 class BlogForm(ModelForm):
     class Meta: 
         model = Blog
-        fields = ('title', 'description', 'date', 'blog_image')
+        fields = ('title', 'description', 'date', 'author', 'blog_image')
         labels = {
             'title' : 'Blog Title',
             'description' : 'Description',
             'date' : 'Date',
+            'author' : 'Written By',
             'blog_image' : 'Uplaod Image',
+            
         }
         widgets = {
             'description': forms.Textarea(attrs={'rows': 6, 'cols': 50}), 
--- blogs/models.py
+++ blogs/models.py
@@ -8,6 +8,7 @@ class Blog(models.Model):
     title = models.CharField(max_length=122) 
     description = models.TextField()
     date = models.DateField(default=date.today, null=True, blank=True)
+    author = models.CharField(max_length=122)
     blog_image = models.ImageField(null=True,blank=True, upload_to= "images/" )
 
     
